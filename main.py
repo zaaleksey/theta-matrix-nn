1ec9b07e5dfdabfa201cc1059a77ab42f8c6c55b
Aleksey Zavarzin <alex.zavr.27100@gmail.com>
2023-03-15 22:30:35 +0400

    Выравнивание вероятностных коэффициентов при получение отрицательных значений. Ограничение максимального количества итераций. Прогресс бар

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import numpy as np
 
-from theta_formation import find_routing_matrix
+from theta_formation import find_routing_matrix, get_uniform_initial_theta
 
 if __name__ == '__main__':
     inp_omega = np.array([.35, .27, .15, .23])
 
-    W = np.array([
+    w = np.array([
         [0, 1, 1, 1],
         [1, 0, 1, 0],
         [0, 0, 0, 1],
         [1, 1, 0, 0]
     ])
 
-    Theta, out_omega = find_routing_matrix(inp_omega, W, log_step=100)
+    # приближенное значение
+    # w = np.array([
+    #     [0, 0, 1, 1],
+    #     [1, 0, 1, 1],
+    #     [0, 0, 0, 1],
+    #     [1, 1, 0, 0]
+    # ])
+
+    Theta, out_omega = find_routing_matrix(inp_omega, w, get_uniform_initial_theta)
--- theta_formation.py
+++ theta_formation.py
@@ -1,7 +1,16 @@
+from typing import Callable
+
 import numpy as np
+from numpy import ndarray
+from progress.bar import Bar
 
 
-def find_routing_matrix(omega, W, eps=10 ** (-10), log_step=0):
+def find_routing_matrix(omega: ndarray,
+                        w: ndarray,
+                        get_initial_theta: Callable,
+                        eps: float = 10 ** (-10),
+                        log_step: int = 0,
+                        max_it: int = 10_000_000) -> tuple[ndarray, ndarray]:
     """
     Функция реализующая метод формирования маршрутной матрицы СеМо
     с заданным вектором относительных интенсивностей потоков
@@ -9,79 +18,108 @@ def find_routing_matrix(omega, W, eps=10 ** (-10), log_step=0):
 
     Parameters:
         omega: вектор относительных интенсивностей потоков
-        W: матрица смежности, определяющая топологию сети о
+        w: матрица смежности, определяющая топологию сети о
         бслуживания
+        get_initial_theta: функция, которая должна вернуть начальную матрицу (имеет два параметра)
         eps: точность при определение вектора omega уравнением
-        omega = omega * Theta,
+        omega = omega * theta,
         по умолчанию имеет значение 10 ** (-10)
         log_step: параметр, отвечающая за шаг логирование в
         процессе работы метода,
         если параметр равен 0 - логирование не производится,
         по умолчанию имеет значение 0
+        max_it: максимальное число воозмможных итераций, если решение не будет найдено, будет вывдена приближенныя матрица
 
     Returns:
         Маршрутную матрицу и соответствующий вектор интенсивностей
         потоков.
 
     """
-    # определяем начальную маршрутную матрицу1
-    row_count, col_count = W.shape[0], W.shape[1]
-    Theta = np.zeros((row_count, col_count))
+    bar = Bar("Progress", max=max_it)
 
-    non_zero_elements_count = [0] * row_count
-    for i, row in enumerate(W):
-        non_zero_elements_count[i] = sum(row)
+    # шаг 1: определяем начальную маршрутную матрицу
+    theta = get_initial_theta(w, omega)
+    print("Омега: ", omega)
+    print("Начальная маршрутная матрица theta имеет следующиий вид:\n", theta, "\n")
 
+    row_count, col_count = theta.shape[0], theta.shape[1]
+
+    # шаг 2: определяем элементы маршрутной матрицы, которые не должны меняться при формировании
+    fixed = set()
     for i in range(row_count):
         for j in range(col_count):
-            if W[i][j] == 1:
-                Theta[i][j] = 1 / non_zero_elements_count[i]
-
-    # определяем компоненты маршрутной матрицы,
-    # которые не должны изменяться
-    fixed = set()
-    for i in range(Theta.shape[0]):
-        for j in range(Theta.shape[1]):
-            # нулевые и единичные компоненты должны быть фиксированы
-            if Theta[i][j] == 0 or Theta[i][j] == 1:
+            if theta[i][j] == 0 or theta[i][j] == 1:  # нулевые и единичные элементы фиксированы
                 fixed |= {(i, j)}
 
     it = 0
-    out_omega = omega.dot(Theta)
-    while any(list(abs(x) > eps for x in out_omega - omega)):
+    out_omega = omega.dot(theta)
+    # итерационно, пока не будет достигнута минимальная погрешность, производится изменение весовых коэффициентов
+    while any(list(abs(x) > eps for x in out_omega - omega)) and it < max_it:
         it += 1
-        out_omega = omega.dot(Theta)
-        error = np.zeros(len(omega))
-        delta = np.zeros(len(omega))
-        for i in range(len(omega)):
-            delta[i] = out_omega[i] - omega[i]
+        bar.next()
+
+        out_omega = omega.dot(theta)
+        error = np.zeros(col_count)
+        delta = np.zeros(col_count)
+        for i in range(col_count):
+            delta[i] = (out_omega[i] - omega[i])
             error[i] = delta[i] ** 2
 
         weight_deltas = np.outer(omega, delta)
-        for i in range(len(weight_deltas)):
-            for j in range(len(weight_deltas)):
+        for i in range(row_count):
+            for j in range(col_count):
                 if (i, j) in fixed:
                     weight_deltas[i][j] = 0
 
-        # корректируем маршрутную матрицу
-        Theta -= weight_deltas
+        # шаг 3: корректируем маршрутную матрицу
+        theta -= weight_deltas
 
-        # нормализуем строки маршрутной матрицы
-        for i in range(Theta.shape[0]):
-            s = sum(Theta[i])
-            for j in range(Theta.shape[1]):
-                Theta[i][j] /= s
+        # шаг 4: нормализуем строки маршрутной матрицы
+        for i in range(row_count):
+            s = sum(theta[i])
+            for j in range(col_count):
+                theta[i][j] /= s
+
+        if np.min(theta) < 0:
+            for i in range(row_count):
+                for j in range(col_count):
+                    if (i, j) not in fixed:
+                        theta[i][j] += (abs(np.min(theta)) * 2)
+
+        # шаг 4: нормализуем строки маршрутной матрицы
+        for i in range(row_count):
+            s = sum(theta[i])
+            for j in range(col_count):
+                theta[i][j] /= s
 
         if log_step and it % log_step == 0:
             print(f"Итерация {it}:")
             print("Полученная омега:", out_omega)
             print("Delta:", delta)
             print("Error:", error)
-            print("Theta\n", Theta, "\n")
+            print("Theta\n", theta, "\n")
+
+    bar.finish()
 
     print(f"Итерация {it} (последняя):")
     print("Полученная омега:", out_omega)
-    print("Delta:", delta)
-    print("Error:", error)
-    print("Theta\n", Theta, "\n")
-    return Theta, out_omega
+    print("Разница омег:", delta)
+    print("Ошибка:", error)
+    print("Theta\n", theta, "\n")
+    return theta, out_omega
+
+
+def get_uniform_initial_theta(w: ndarray, omega: ndarray) -> ndarray:
+    row_count, col_count = w.shape[0], w.shape[1]
+    theta = np.zeros((row_count, col_count))
+
+    non_zero_elements = [0] * row_count
+    for i, row in enumerate(w):
+        non_zero_elements[i] = sum(row)
+
+    for i in range(row_count):
+        for j in range(col_count):
+            if w[i][j] == 1:
+                theta[i][j] = 1 / non_zero_elements[i]  # равномерное распределние элементов матрицы
+
+    return theta
